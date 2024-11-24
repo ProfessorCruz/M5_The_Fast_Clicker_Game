69dc710724d45eaa56653f7ec44da58c09c86c8a
ProfessorCruz <reneecruz@ua.pt>
2024-11-24 19:03:36 +0000

    Original code, by Algorithmics Global

--- The_Fast_Clicker_Game.py
+++ The_Fast_Clicker_Game.py
@@ -43,3 +43,95 @@ cur_time = start_time
 
 
 ''' Game interface'''
+time_text = Label(0,0,50,50,back)
+time_text.set_text('Time:',40, DARK_BLUE)
+time_text.draw(20, 20)
+
+
+timer = Label(50,55,50,40,back)
+timer.set_text('0', 40, DARK_BLUE)
+timer.draw(0,0)
+
+
+score_text = Label(380,0,50,50,back)
+score_text.set_text('Count:',45, DARK_BLUE)
+score_text.draw(20,20)
+
+
+score = Label(430,55,50,40,back)
+score.set_text('0', 40, DARK_BLUE)
+score.draw(0,0)
+
+
+for i in range(num_cards):
+  new_card = Label(x, 170, 70, 100, YELLOW)
+  new_card.outline(BLUE, 10)
+  new_card.set_text('CLICK', 26)
+  cards.append(new_card)
+  x = x + 100
+wait = 0
+points = 0
+from random import randint
+while True:
+  '''Drawing cards and displaying clicks'''
+  if wait == 0:
+      wait = 20 #so many ticks the label will be in one place
+      click = randint(1, num_cards)
+      for i in range(num_cards):
+          cards[i].color(YELLOW)
+          if (i + 1) == click:
+              cards[i].draw(10, 40)
+          else:
+              cards[i].fill()
+  else:
+      wait -= 1
+  '''Handling clicks on cards'''
+  for event in pygame.event.get():
+      if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+          x, y = event.pos
+          for i in range(num_cards):
+              if cards[i].collidepoint(x,y):
+                  if i + 1 == click:# if there is a label on the card, we color it green, add a point 
+                      cards[i].color(GREEN)
+points += 1
+                  else: #otherwise color it red, minus a point
+                      cards[i].color(RED)
+                      points -= 1
+                  cards[i].fill()
+                  score.set_text(str(points),40, DARK_BLUE)
+                  score.draw(0,0)
+  '''Winning and losing'''
+  new_time = time.time()
+
+
+  if new_time - start_time  >= 11:
+       win = Label(0, 0, 500, 500, LIGHT_RED)
+       win.set_text("Time is up!!!", 60, DARK_BLUE)
+       win.draw(110, 180)
+       break
+  
+  if int(new_time) - int(cur_time) == 1: #check if there is a difference of 1 second between the old and new time
+       timer.set_text(str(int(new_time - start_time)),40, DARK_BLUE)
+       timer.draw(0,0)
+       cur_time = new_time
+
+
+  if points >= 5:
+       win = Label(0, 0, 500, 500, LIGHT_GREEN)
+       win.set_text("You won!!!", 60, DARK_BLUE)
+       win.draw(140, 180)
+       resul_time = Label(90, 230, 250, 250, LIGHT_GREEN)
+       resul_time.set_text("Time to complete: " + str (int(new_time - start_time)) + " сек", 40, DARK_BLUE)
+
+
+       resul_time.draw(0, 0)
+
+
+       break
+
+
+  pygame.display.update()
+  clock.tick(40)
+
+
+pygame.display.update() 
